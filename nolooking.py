77e3b7db2b5b80f23bd8e1595c0826b8a275dc15
A731 <audunelv@hotmail.com>
2017-05-29 17:14:55 +0200

    Python3 support, bugfix

--- nolooking.py
+++ nolooking.py
@@ -1,32 +1,16 @@
-#Spare me
-/usr/bin/env python
+#!/usr/bin/env python
 
 import os
 from os import system
 import curses
 import glob
-import time
-
-def get_param(prompt_string):
-     screen.clear()
-     screen.border(0)
-     screen.addstr(2, 2, prompt_string)
-     screen.refresh()
-     input = screen.getstr(10, 10, 60)
-     return input
-
-def execute_cmd(cmd_string):
-     system("clear")
-     a = system(cmd_string)
-     print ""
-     if a == 0:
-          print "Command executed correctly"
-     else:
-          print "Command terminated with error"
-     raw_input("Press enter")
-     print ""
+import time 
+import RPi.GPIO as GPIO
 
 svar = 0
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
 
 while svar != ord('4'):
      screen = curses.initscr()
@@ -43,12 +27,10 @@ while svar != ord('4'):
      svar = screen.getch()
 
      if svar == ord('1'):
-         GPIO.output(18,GPIO.HIGH)
-         time.sleep(0.2)
-         GPIO.output(18,GPIO.LOW)
+        GPIO.output(18,GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(18,GPIO.LOW)
      if svar == ord('2'):
-        curses.endwin()
-        screen.clear()
         os.system('modprobe w1-gpio')
         os.system('modprobe w1-therm')
         base_dir = '/sys/bus/w1/devices/'
@@ -56,7 +38,7 @@ while svar != ord('4'):
         device_file = device_folder + '/w1_slave'
         def read_temp_raw():
             f = open(device_file, 'r')
-            lines = f.readlines()
+       	    lines = f.readlines()
             f.close()
             return lines
         def read_temp():
@@ -67,20 +49,24 @@ while svar != ord('4'):
             equals_pos = lines[1].find('t=')
             if equals_pos != -1:
                 temp_string = lines[1][equals_pos+2:]
-                temp_c = float(temp_string) / 1000.0
+       	        temp_c = float(temp_string) / 1000.0
                 temp_f = temp_c * 9.0 / 5.0 + 32.0
-                return temp_c #, temp_f  #uncomment for fahrenheit
+                return temp_c #add temp_f for fahrenheit
+        timeout = time.time() + 5 
         while True:
-               curses.endwin()
-               screen.clear()               
-               screen.addstr(4, 6, "" + str(int(read_temp())))
-               screen.refresh
-               time.sleep(1)
-     if svar == ord('3'):
+         if time.time() > timeout:
+           break
+         screen.clear()
+         screen.addstr(4, 6, str(int(read_temp())) + "C" ) 
+         screen.refresh()
+        
+     if  svar == ord('3'):
           curses.endwin()
           screen.clear()
           screen.addstr(4, 6, "This is not a teapot")
           screen.refresh()
-          time.sleep(5)
+          time.sleep(5) 
 
 curses.endwin()
+
+
