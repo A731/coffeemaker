69a59c9cda08f3c36446fd6df81b41bfdc5a3046
A731 <audunelv@hotmail.com>
2017-05-29 17:23:01 +0200

    Ultrasonic sensor

---
+++ Avstandssensor python.py
@@ -0,0 +1,56 @@
+import RPi.GPIO as GPIO
+import time
+ 
+#Viktig
+GPIO.setmode(GPIO.BCM)
+ 
+GPIO_TRIGGER = 23
+GPIO_ECHO = 24
+GPIO_PUMP = 17
+ 
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+GPIO.setup(GPIO_PUMP, GPIO.OUT)
+def distance():
+    # Trigger høg
+    GPIO.output(GPIO_TRIGGER, True)
+ 
+    # 0.01ms = låg
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+ 
+    StartTid = time.time()
+    StoppTid = time.time()
+ 
+    # lagre StartTid
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTid = time.time()
+ 
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StoppTid = time.time()
+ 
+    # time difference between start and arrival
+    TimeElapsed = StoppTid - StartTid
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+ 
+    return distance
+ 
+
+if distance()>=5: #5 er eksempelverdi
+	GPIO.output(GPIO_PUMP, True)
+	time.wait(5)
+
+#if __name__ == '__main__':
+#    try:
+#        while True:
+#            dist = distance()
+#            print ("Measured Distance = %.1f cm" % dist)
+#            time.sleep(1)
+# 
+#        # Reset by pressing CTRL + C
+#    except KeyboardInterrupt:
+#        print("Measurement stopped by User")
+#        GPIO.cleanup()
